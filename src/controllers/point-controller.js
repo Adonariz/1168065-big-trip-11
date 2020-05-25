bdd3ffe65094d02852fe729d48f5b63ab7133727
A Zhila <aleksej.zhila@gmail.com>
2020-05-25 17:36:36 +0300

    Добавляет перерисовку евента

--- src/controllers/point-controller.js
+++ src/controllers/point-controller.js
@@ -3,6 +3,11 @@ import {ESC_KEY} from "../utils/const";
 import EventItem from "../components/page-main/events/event-item";
 import EventEdit from "../components/page-main/events/event-edit";
 
+const ViewMode = {
+  DEFAULT: `default`,
+  EDIT: `edit`,
+};
+
 export default class PointController {
   constructor(container, onDataChange) {
     this._container = container;
@@ -11,21 +16,26 @@ export default class PointController {
     this._eventComponent = null;
     this._eventEditComponent = null;
 
+    this._viewMode = ViewMode.DEFAULT;
+
     this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
   render(event) {
+    const oldEventComponent = this._eventComponent;
+    const oldEventEditComponent = this._eventEditComponent;
+
     this._eventComponent = new EventItem(event);
     this._eventEditComponent = new EventEdit(event);
 
     this._eventComponent.setEditButtonClickHandler(() => {
-      this._replaceTaskToEdit();
+      this._replaceEventToEdit();
       document.addEventListener(`keydown`, this._onEscKeyDown);
     });
 
     this._eventEditComponent.setSubmitHandler((evt) => {
       evt.preventDefault();
-      this._replaceEditToTask();
+      this._replaceEditToEvent();
     });
 
     this._eventEditComponent.setFavoriteButtonHandler(() => {
@@ -34,21 +44,35 @@ export default class PointController {
       }));
     });
 
-    render(this._container, this._eventComponent, RenderPosition.BEFOREEND);
+    if (oldEventComponent && oldEventEditComponent) {
+      replace(this._eventComponent, oldEventComponent);
+      replace(this._eventEditComponent, oldEventEditComponent);
+    } else {
+      render(this._container, this._eventComponent, RenderPosition.BEFOREEND);
+    }
+
   }
 
-  _replaceEditToTask() {
+  setDefaultView() {
+    if (this._viewMode !== ViewMode.DEFAULT) {
+      this._replaceEditToEvent();
+    }
+  }
+
+  _replaceEditToEvent() {
     document.removeEventListener(`keydown`, this._onEscKeyDown);
     replace(this._eventComponent, this._eventEditComponent);
+    this._viewMode = ViewMode.DEFAULT;
   }
 
-  _replaceTaskToEdit() {
+  _replaceEventToEdit() {
     replace(this._eventEditComponent, this._eventComponent);
+    this._viewMode = ViewMode.EDIT;
   }
 
   _onEscKeyDown(evt) {
     if (evt.key === ESC_KEY) {
-      this._replaceEditToTask();
+      this._replaceEditToEvent();
       document.removeEventListener(`keydown`, this._onEscKeyDown);
     }
   }
